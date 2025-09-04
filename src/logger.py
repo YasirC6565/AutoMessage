3dfd2484e20ebafc261749cdcdb7a74f75c268b0
KarthikR727 <karthik02.ramesh@gmaile.com>
2025-09-04 19:25:12 +0100

    completed logger for MVP1

--- src/logger.py
+++ src/logger.py
@@ -8,3 +8,20 @@ Functions to implement:
     Prints timestamped logs to the console (later can be replaced with proper logging).
 """
 
+from datetime import datetime
+
+def log(message: str) -> None:
+    # get the current time
+    timestamp = datetime.now()
+
+    # print the time and the message
+    print(f"[{timestamp}] {message}")
+
+
+#testing code
+from logger import log
+
+# Try logging different messages
+log("Program started")
+log("Loading input cleaner")
+log("Process finished successfully")
